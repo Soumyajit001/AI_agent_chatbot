c2cd9272703b85540ed46cb70bac757d79d57602
Soumyajit001 <soumyajitpal416@gmail.com>
2025-03-29 13:27:04 +0530

    FastAPI.py and frontend.py updated

--- frontend.py
+++ frontend.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import requests
 
 st.set_page_config(page_title = "LangGraph Agent UI", layout="centered")
 st.title("AI Chatbot Agents")
@@ -7,7 +6,7 @@ st.write("Create and Interact with the AI Agents!")
 
 system_prompt = st.text_area("Tell about your AI Agent: ", height=70, placeholder="Type your system prompt here...")
 
-MODEL_NAME_GROQ = ["llama-3.3-70b-versatile", "mixtral-8x7b-32768"]
+MODEL_NAME_GROQ = ["llama-3.3-70b-versatile", "llama3-70b-8192", "mixtral-8x7b-32768"]
 MODEL_NAME_OPENAI = ["gpt-4o-mini"]
 
 provider = st.radio("Select Provider:", ("Groq", "OpenAI"))
@@ -51,6 +50,7 @@ API_URL = "http://127.0.0.1:9999/chat"
 
 if st.button("Ask Agent!"):
     if user_query.strip():
+        import requests
         payload = {
             "model_name": selected_model,
             "model_provider": provider,
@@ -69,8 +69,9 @@ if st.button("Ask Agent!"):
                 st.error(response_data["error"])
             else:
                 st.subheader("Agent Response")
-                agent_response = response_data.get("response", "No response field found in API data")
-                st.markdown(f"**Final Response:** {agent_response}")
+                # agent_response = response_data.get("response", "No response field found in API data")
+                # st.markdown(f"**Final Response:** {agent_response}")
+                st.markdown(f"**Final Response:** {response}")
         else:
             st.error(f"API request failed with status code: {response.status_code}")
     else:
